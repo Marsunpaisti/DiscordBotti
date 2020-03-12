ab5b87c3f0222a64804dc10e4c15d55edd1db60f
Aleksi Kytölä <marsunpaisti@gmail.com>
2020-03-12 20:58:29 +0200

    winner checking

--- modules/betting.js
+++ modules/betting.js
@@ -1,6 +1,9 @@
 const Enmap = require("enmap");
 const axios = require("axios");
+const schedule = require("node-schedule");
 const betDatabase = new Enmap({ name: "coronaBetDatabase" });
+const channelDatabase = new Enmap({ name: "coronaReportingChannels" });
+const scoreDatabase = new Enmap({ name: "coronaBettingScoreDatabase" });
 
 const addBet = async (client, message, bet) => {
 	let serverDatabase = betDatabase.get(message.guild.id);
@@ -40,7 +43,7 @@ const addBet = async (client, message, bet) => {
 	return message.reply(`${actionString} your bet to ${bet} corona cases tomorrow (${dateString})!`);
 };
 
-const getCurrentBets = (client, message) => {
+const getCurrentBets = async (client, message) => {
 	let serverDatabase = betDatabase.get(message.guild.id);
 	if (!serverDatabase) return message.channel.send(`No bets at the moment`);
 
@@ -55,7 +58,7 @@ const getCurrentBets = (client, message) => {
 		messageString += "Bets for today:\n";
 		for (let key in todaysBets) {
 			let bet = todaysBets[key];
-			let userName = client.users.fetch(bet.userId).username || bet.username;
+			let userName = (await client.users.fetch(bet.userId).username) || bet.username;
 			messageString += userName + " is betting for:\t\t\t" + bet.bet + " cases\n";
 		}
 	} else {
@@ -68,7 +71,7 @@ const getCurrentBets = (client, message) => {
 		messageString += "Bets for tomorrow:\n";
 		for (let key in tomorrowBets) {
 			let bet = tomorrowBets[key];
-			let userName = client.users.fetch(bet.userId).username || bet.username;
+			let userName = (await client.users.fetch(bet.userId).username) || bet.username;
 			messageString += userName + " is betting for:\t\t\t" + bet.bet + " cases\n";
 		}
 	}
@@ -77,7 +80,7 @@ const getCurrentBets = (client, message) => {
 	return message.channel.send(messageString);
 };
 
-const checkBets = async client => {
+const checkWinners = async client => {
 	let coronaData = null;
 	for (let attempt = 1; attempt <= 10; attempt++) {
 		try {
@@ -94,13 +97,87 @@ const checkBets = async client => {
 	}
 
 	const totalCases = coronaData.confirmed.length;
-	client.logger.debug("Total cases: " + totalCases);
+
+	let guildIds = betDatabase.keyArray();
+	guildIds.forEach(guildId => {
+		checkWinnersForGuild(client, guildId, totalCases);
+	});
 };
+
+const checkWinnersForGuild = async (client, guildId, totalCases) => {
+	//Check bets
+	let serverDatabase = betDatabase.get(guildId);
+	if (!serverDatabase) return;
+
+	const date = new Date();
+	date.setDate(date.getDate() + 1);
+	const todayDateString = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+
+	const todaysBets = serverDatabase[todayDateString];
+	if (!todaysBets) return;
+
+	let guessDifferences = {};
+	for (let key in todaysBets) {
+		let bet = todaysBets[key];
+		let difference = Math.abs(totalCases - bet.bet);
+		if (!(difference in guessDifferences)) guessDifferences[difference] = [];
+		guessDifferences[difference].push(bet);
+	}
+
+	let sortedKeys = Object.keys(guessDifferences).sort();
+	let message = "Current corona total: " + totalCases + " confirmed cases\n";
+	message += "The winners of coronabingo today are:\n";
+	let guessesArray = guessDifferences[sortedKeys[0]];
+	for (let bet of guessesArray) {
+		let username = (await client.users.fetch(bet.userId).username) || bet.username;
+		message += `**${username}** won with a bet of ${bet.bet}\n`;
+		let prevScore = scoreDatabase.get(bet.userId);
+		if (!prevScore) {
+			scoreDatabase.set(bet.userId, 1);
+		} else {
+			scoreDatabase.set(bet.userId, prevScore + 1);
+		}
+	}
+	message += "------------------------------------------------------------\n";
+	message += "TOP 5 Guesses:\n";
+	let index = 0;
+	for (let key of sortedKeys) {
+		index += 1;
+		guessesArray = guessDifferences[key];
+		message += `${index}:`;
+		for (let bet of guessesArray) {
+			let username = (await client.users.fetch(bet.userId).username) || bet.username;
+			message += `**${username}** (${bet.bet}) `;
+		}
+		message += `\n`;
+	}
+
+	message += "------------------------------------------------------------\n";
+	let channelId = channelDatabase.get(guildId);
+	if (!channelId) return;
+	let channel = client.channels.resolve(channelId);
+	if (!channel) return;
+	channel.send(message);
+};
+
+function setChannel(client, message) {
+	channelDatabase.set(message.guild.id, message.channel.id);
+	client.logger.log(
+		`Set channel ${message.channel.name} (${message.channel.id}) as corona betting channel for guild ${message.guild.name} (${message.guild.id})`
+	);
+
+	return message.channel.send(`Corona betting winners will now be announced on this channel!`);
+}
 
 exports.init = async client => {
 	client.logger.log(`\tLoading corona bet database`);
 	await betDatabase.defer;
 	client.logger.log(`\tCorona bet database loaded`);
+	const scheduleRule = new schedule.RecurrenceRule();
+	scheduleRule.hour = 18;
+	schedule.scheduleJob(scheduleRule, () => {
+		checkWinners(client);
+	});
 };
 
 exports.run = async (client, message, command, args) => {
@@ -114,14 +191,24 @@ exports.run = async (client, message, command, args) => {
 		addBet(client, message, bet);
 	} else if (command == "getbets" || command == "currentbets" || command == "bets") {
 		getCurrentBets(client, message);
-	} else if (command == "check") {
-		checkBets(client);
+	} else if (command == "setbettingchannel" || command == "setcoronachannel") {
+		setChannel(client, message);
 	}
 };
 
 exports.config = {
 	enabled: true,
-	commands: ["addbet", "bet", "coronabet", "cases", "getbets", "currentbets", "bets", "check"],
+	commands: [
+		"addbet",
+		"bet",
+		"coronabet",
+		"cases",
+		"getbets",
+		"currentbets",
+		"bets",
+		"setbettingchannel",
+		"setcoronachannel"
+	],
 	allowPrivateMessages: false
 };
 
@@ -129,5 +216,6 @@ exports.info = {
 	name: "Betting",
 	category: "Fun",
 	description: "Lets users bet for coronavirus cases for the next day, keeping score",
-	usage: "\nbet [numberOfCases] to bet\nbets to get current bets\ncases to get current statistics on coronavirus"
+	usage:
+		"\n**bet** [numberOfCases] to bet\n**bets** to get current bets\n**cases** to get current statistics on coronavirus\n**setbettingchannel** to set the current channel for winner reporting"
 };
